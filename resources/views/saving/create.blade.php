f6a82c4f6cd0cdb6bde760073a19ec947e5f7282
elissamunanira <munanira64@gmail.com>
2023-04-21 11:42:10 +0200

    saving

--- resources/views/saving/create.blade.php
+++ resources/views/saving/create.blade.php
@@ -1,20 +1,55 @@
 @extends('dashboard.app')
 @section('content')
 
-<form method="POST" action="{{ route('savings.store') }}">
-    @csrf
-    <div class="form-group">
-        <label for="user_id">User ID:</label>
-        <input type="text" name="user_id" class="form-control" required>
+
+<div class="content-wrap">
+    <div class="main">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-8 p-r-0 title-margin-right">
+                    <div class="page-header">
+                        <div class="page-title">
+                            <h1>Hello, <span>Welcome to Role Management Field</span></h1>
+                        </div>
+                    </div>
+                </div>
+                <!-- /# column -->
+                <div class="col-lg-4 p-l-0 title-margin-left">
+                    <div class="page-header">
+                        <div class="page-title">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="/dash">Dashboard</a></li>
+                                <li class="breadcrumb-item active">Roles</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+                <!-- /# column -->
+            </div>
+            <!-- /# row -->
+            <section id="main-content">
+                <div class="row">
+                    <div class="container">
+                        <form method="POST" action="{{ route('savings.store') }}">
+                            @csrf
+                            <div class="form-group">
+                                <label for="user_id">User ID:</label>
+                                <input type="text" name="user_id" class="form-control" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="amount">Amount:</label>
+                                <input type="number" name="amount" class="form-control" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="month">Month:</label>
+                                <input type="date" name="month" class="form-control" required>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </section>
+        </div>
     </div>
-    <div class="form-group">
-        <label for="amount">Amount:</label>
-        <input type="number" name="amount" class="form-control" required>
-    </div>
-    <div class="form-group">
-        <label for="month">Month:</label>
-        <input type="date" name="month" class="form-control" required>
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+</div>
 @endsection
--- resources/views/saving/saving.blade.php
+++ resources/views/saving/saving.blade.php
@@ -1,23 +1,55 @@
 @extends('dashboard.app')
 
 @section('content')
-    <div class="container">
-        <h1>Savings records for {{ $user->name }}</h1>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Amount</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($savings as $saving)
-                    <tr>
-                        <td>{{ $saving->month }}</td>
-                        <td>{{ $saving->amount }}</td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
+<div class="content-wrap">
+    <div class="main">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-8 p-r-0 title-margin-right">
+                    <div class="page-header">
+                        <div class="page-title">
+                            <h1>Hello, <span>Welcome to Role Management Field</span></h1>
+                        </div>
+                    </div>
+                </div>
+                <!-- /# column -->
+                <div class="col-lg-4 p-l-0 title-margin-left">
+                    <div class="page-header">
+                        <div class="page-title">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="/dash">Dashboard</a></li>
+                                <li class="breadcrumb-item active">Roles</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+                <!-- /# column -->
+            </div>
+            <!-- /# row -->
+            <section id="main-content">
+                <div class="row">
+                    <div class="container">
+                        <h1>Savings records for {{ $user->name }}</h1>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>Date</th>
+                                    <th>Amount</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($savings as $saving)
+                                    <tr>
+                                        <td>{{ $saving->month }}</td>
+                                        <td>{{ $saving->amount }}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </section>
+        </div>
     </div>
+</div>
 @endsection
