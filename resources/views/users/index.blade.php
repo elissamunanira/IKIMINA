143b13f69082b0f6a44284e0b7bf033cfa791fcf
elissamunanira <munanira64@gmail.com>
2023-04-22 21:01:51 +0200

    saving

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -20,7 +20,7 @@
         <th>No</th>
         <th>Name</th>
         <th>Email</th>
-        <th>Roles</th>
+        <th>Role</th>
         <th width="280px">Action</th>
     </tr>
 @foreach ($data as $key => $user)
@@ -156,25 +156,23 @@
                                                         <div class="dropdown text-sans-serif"><i class="tp-btn-light sharp" type="button" id="order-dropdown-0" data-toggle="dropdown" data-boundary="viewport" aria-haspopup="true" aria-expanded="false"><span><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="18px" height="18px" viewBox="0 0 24 24" version="1.1"><g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><rect x="0" y="0" width="24" height="24"></rect><circle fill="#000000" cx="5" cy="12" r="2"></circle><circle fill="#000000" cx="12" cy="12" r="2"></circle><circle fill="#000000" cx="19" cy="12" r="2"></circle></g></svg></span></i>
                                                             <div class="dropdown-menu dropdown-menu-right border py-0" aria-labelledby="order-dropdown-0">
                                                                 @can('user-list')
-                                                                <div class="py-2"><a class="dropdown-item" href="{{ route('users.show',$user->id) }}">Show</a>  
-                                                                @endcan
-                                            
-                                            
+                                                                <div class="py-2"><a class="dropdown-item" href="{{ route('users.show',$user->id) }}">View</a>
+                                                                    @endcan
+    
+    
                                                                     <div class="dropdown-divider"></div>
-                                            
-                                                                    
+    
                                                                     @can('user-edit')
                                                                     <a class="dropdown-item" href="{{ route('users.edit',$user->id) }}">Edit</a>
                                                                     @endcan
-                                            
+    
                                                                     <div class="dropdown-divider"></div>
-                                                                    
                                                                     @can('user-delete')
                                                                     {!! Form::open(['method' => 'DELETE','route' => ['users.destroy', $user->id],'style'=>'display:inline']) !!}
                                                                         {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
                                                                     {!! Form::close() !!}
+                                                                
                                                                     @endcan
-                                                                </div>
                                                                 </div>
                                                             </div>
                                                         </div>
