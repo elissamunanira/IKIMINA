303f3ca4ca8dfa81c6f7f7cef8b6df65e0ee385e
elissamunanira <munanira64@gmail.com>
2023-04-23 18:30:34 +0200

    users

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,7 +1,44 @@
 @extends('dashboard.app')
 @section('content')
-<div class="container">
-<div class="row">
+
+<div class="content-wrap">
+    <div class="main">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-8 p-r-0 title-margin-right">
+                    <div class="page-header">
+                        <div class="page-title">
+                            <h1>Hello, <span>Welcome to Users Management</span></h1>
+                        </div>
+                    </div>
+                </div>
+                <!-- /# column -->
+                <div class="col-lg-4 p-l-0 title-margin-left">
+                    <div class="page-header">
+                        <div class="page-title">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="/dash">Dashboard</a></li>
+                                <li class="breadcrumb-item active">Users</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+                <!-- /# column -->
+            </div>
+
+
+            <div class="row">
+                <div class="col-lg-12 margin-tb">
+                    <div class="pull-left">
+                        <a class="btn btn-primary" href="{{ route('users.create') }}"> Create New User</a>
+                    </div>
+                </div>
+            </div>
+
+
+            <!-- /# row -->
+            <section id="main-content">
+                <div class="row">
     <div class="col-lg-12 margin-tb">
         <div class="pull-left">
             <h2> Show User</h2>
@@ -35,5 +72,18 @@
         </div>
     </div>
 </div>
+<!-- /# row -->
+
+<div class="row">
+    <div class="col-lg-12">
+        <div class="footer">
+            <p>2023 © Admin Board. - <a href="#">Ikibina</a></p>
+        </div>
+    </div>
 </div>
+</section>
+</div>
+</div>
+</div>
+
 @endsection
