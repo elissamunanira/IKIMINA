80511b04625be49dca135b9f519d6f0873f758b8
elissamunanira <munanira64@gmail.com>
2023-04-27 18:28:17 +0200

    loan

---
+++ resources/views/loans/index.blade.php
@@ -0,0 +1,55 @@
+@extends('dashboard.app')
+
+@section('content')
+<div class="content-wrap">
+    <div class="main">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-8 p-r-0 title-margin-right">
+                    <div class="page-header">
+                        <div class="page-title">
+                            <h1>Hello, <span>Welcome to Saving Management Field</span></h1>
+                        </div>
+                    </div>
+                </div>
+                <!-- /# column -->
+                <div class="col-lg-4 p-l-0 title-margin-left">
+                    <div class="page-header">
+                        <div class="page-title">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="/dash">Dashboard</a></li>
+                                <li class="breadcrumb-item active">Savings</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+                <!-- /# column -->
+            </div>
+            <!-- /# row -->
+            <section id="main-content">
+                <div class="row">
+                    <div class="container">
+                        <h3>Savings records for <strong>{{ $user->firstname }} {{ $user->lastname }}</strong></h3>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>Date</th>
+                                    <th>Amount</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($savings as $saving)
+                                    <tr>
+                                        <td>{{ $saving->month }}</td>
+                                        <td>{{ $saving->amount }}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+</div>
+@endsection
