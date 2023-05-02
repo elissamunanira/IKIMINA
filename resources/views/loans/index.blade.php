2679c43642ec751d30f0f078b4d63d54ad1b3df0
elissamunanira <munanira64@gmail.com>
2023-05-02 22:17:53 +0200

    loan

--- resources/views/loans/index.blade.php
+++ resources/views/loans/index.blade.php
@@ -59,16 +59,15 @@
                                                 <span class="badge rounded-pill bg-info">paid</span>
                                             @endif
                                         </td>
-                                        <td>
-                                           <div class="dropdown text-sans-serif"><i class="tp-btn-light sharp" type="button" id="order-dropdown-0" data-toggle="dropdown" data-boundary="viewport" aria-haspopup="true" aria-expanded="false"><span><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="18px" height="18px" viewBox="0 0 24 24" version="1.1"><g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><rect x="0" y="0" width="24" height="24"></rect><circle fill="#000000" cx="5" cy="12" r="2"></circle><circle fill="#000000" cx="12" cy="12" r="2"></circle><circle fill="#000000" cx="19" cy="12" r="2"></circle></g></svg></span></i>
-                                                <div class="dropdown-menu dropdown-menu-right border py-0" aria-labelledby="order-dropdown-0"> 
-                                                        <div class="dropdown-divider"></div> 
-                                                        @can('role-edit')
+                                        <td> <a href="{{ route('loan.edit',$loan->id) }}">Action</a>
+                                           {{-- <div class="dropdown text-sans-serif"><i class="tp-btn-light sharp" type="button" id="order-dropdown-0" data-toggle="dropdown" data-boundary="viewport" aria-haspopup="true" aria-expanded="false"><span><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="18px" height="18px" viewBox="0 0 24 24" version="1.1"><g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><rect x="0" y="0" width="24" height="24"></rect><circle fill="#000000" cx="5" cy="12" r="2"></circle><circle fill="#000000" cx="12" cy="12" r="2"></circle><circle fill="#000000" cx="19" cy="12" r="2"></circle></g></svg></span></i>
+                                                <div class="dropdown-menu dropdown-menu-right border py-0" aria-labelledby="order-dropdown-0">  
+                                                        @can('loan-approval')
                                                         <a class="dropdown-item" href="{{ route('loan.edit',$loan->id) }}">Edit</a>
                                                         @endcan 
                                                     </div>
                                                 </div>
-                                            </div>
+                                            </div> --}}
                                         </td>
                                     </tr>
                                 @endforeach
