10a28f888ffc1b402b1056f6299f7fe4500337b5
elissamunanira <munanira64@gmail.com>
2023-05-07 13:43:49 +0200

    budget line description

--- resources/views/budgetlines/index.blade.php
+++ resources/views/budgetlines/index.blade.php
@@ -67,6 +67,7 @@
                                                                 <th>#</th>
                                                                 <th>Name</th>
                                                                 <th>Amount</th>
+                                                                <th>Description</th>
                                                                 <th>Actions</th>
                                                             </tr>
                                                         </thead>
@@ -76,6 +77,8 @@
                                                                     <td>{{ ++$i }}</td>
                                                                     <td>{{ $budgetLine->budget_line_name }}</td>
                                                                     <td>{{$budgetLine->budget_line_amount }} RWF
+                                                                    <td>
+                                                                    <td>{{$budgetLine->budget_line_description }}
                                                                     <td>
                                                                         <div class="dropdown text-sans-serif"><i class="tp-btn-light sharp" type="button" id="order-dropdown-0" data-toggle="dropdown" data-boundary="viewport" aria-haspopup="true" aria-expanded="false"><span><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="18px" height="18px" viewBox="0 0 24 24" version="1.1"><g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><rect x="0" y="0" width="24" height="24"></rect><circle fill="#000000" cx="5" cy="12" r="2"></circle><circle fill="#000000" cx="12" cy="12" r="2"></circle><circle fill="#000000" cx="19" cy="12" r="2"></circle></g></svg></span></i>
                                                                         <div class="dropdown-menu dropdown-menu-right border py-0" aria-labelledby="order-dropdown-0">
