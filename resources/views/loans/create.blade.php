fab4c2c0a07a86cae13b1bd41ddf3958c82888f7
elissamunanira <munanira64@gmail.com>
2023-05-08 11:31:52 +0200

    loanCategories

--- resources/views/loans/create.blade.php
+++ resources/views/loans/create.blade.php
@@ -18,7 +18,7 @@
                                     <select name="category" class="form-control">
                                         <option value="">--- Select Category ---</option>
                                         @foreach ($categories as $category)
-                                            <option value="{{ $category->id }}">{{ $category->category }} </option>">
+                                            <option value="{{ $category->id }}">{{ $category->name }} </option>">
                                         @endforeach
                                     </select>
                                 </div>
