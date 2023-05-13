9adbf2d0bd818f51eaa6fb56955f7b589c7abab3
elissamunanira <munanira64@gmail.com>
2023-05-13 13:08:35 +0200

    loan payment

--- database/migrations/2023_05_16_102945_create_expenses_table.php
+++ database/migrations/2023_05_16_102945_create_expenses_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
              $table->id();
             $table->unsignedBigInteger('budget_line_id');
             $table->string('expense_name');
+            $table->string('description');
             $table->decimal('expense_amount', 15, 2);
             $table->timestamps();
 
