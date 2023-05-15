1e47191afbd95fd69e49a93b21df24787cc1209b
elissamunanira <munanira64@gmail.com>
2023-05-15 14:58:40 +0200

    penalty

--- app/Http/Controllers/PenaltyController.php
+++ app/Http/Controllers/PenaltyController.php
@@ -31,12 +31,28 @@ class PenaltyController extends Controller
             'description' => 'required', 
         ]);
 
-        $penalty = Penalty::create($validated);
-
-        // Apply the penalty
-        // ...
+        $penalty = Penalty::create($validated); 
 
         return redirect()->route('penalties.index')->with('success','New Penalty created successfully');
+    }
+
+    public function edit($id){
+        $penalties = Penalty::find($id);
+        $users = User::find($id);
+        return view('penalties.edit', compact('penalties'));
+    }
+
+    public function update($id){
+
+        $validated = $request->validate([
+            'user_id' => 'required|exists:users,id',
+            'rule_id' => 'required|exists:rules,id',
+            'description' => 'required', 
+        ]);
+
+        $penalty = Penalty::update($validated); 
+
+        return redirect()->route('penalties.index')->with('success','Penalty updated successfully');
     }
 
     public function pay(Penalty $penalty)
---
+++ resources/views/penalties/edit.blade.php
@@ -0,0 +1,84 @@
+@extends('dashboard.app')
+@section('content')
+<div class="content-wrap">
+    <div class="main">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-8 p-r-0 title-margin-right">
+                    <div class="page-header">
+                        <div class="page-title">
+                            <h1>Add New Penalty</h1>
+                        </div>
+                    </div>
+                </div>
+                <!-- /# column -->
+                <div class="col-lg-4 p-l-0 title-margin-left">
+                    <div class="page-header">
+                        <div class="page-title">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="/dash">Dashboard</a></li>
+                                <li class="breadcrumb-item active">Peanlty</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+                <!-- /# column -->
+            </div>
+            <!-- /# row -->
+            <section id="main-content">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="form-validation">
+
+                                    @if(Session::get('Success'))
+                                        <div class="alert alert-success">
+                                            {{Session::get('Success')}}
+                                            @php 
+                                            Session::forget('Success')
+                                            @endphp
+                                        </div>
+                                        @endif
+                                       <form method="POST" action="{{ route('penalties.store') }}">
+                                        @csrf
+
+                                        <div class="form-group">
+                                            <label for="member_id">Member </label>
+                                            <select class="form-control" id="member_id" name="user_id" value="{{ old('member_id') }}" required>
+                                                    <option selected>--Select Member To assign the penalty</option>
+                                                @foreach($users as $user)
+                                                    <option value="{{ $user->id }}">{{ $user->firstname }} {{ $user->lastname }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+
+                                        <div class="form-group">
+                                            <label for="rule_id">Rule:</label>
+                                            <select class="form-control" id="rule_id" name="rule_id" required>
+                                                    <option selected>--Select Violeted Rule--</option>
+                                                @foreach ($rules as $rule)
+                                                    <option value="{{ $rule->id }}">{{ $rule->name }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+
+                                        <div class="form-group">
+                                            <label for="description">Description:</label>
+                                            <textarea class="form-control" id="description" name="description" rows="3" required>{{ old('description') }}</textarea>
+                                        </div> 
+
+                                        <button type="submit" class="btn btn-primary">Update Penalty</button>
+                                    </form>
+                                    </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </section>
+        </div>
+    </div>
+</div>
+
+@endsection  
