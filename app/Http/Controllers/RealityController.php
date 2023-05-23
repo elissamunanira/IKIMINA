9a9916d678252969f9553365f28f803556c8adac
elissamunanira <munanira64@gmail.com>
2023-05-23 12:13:05 +0200

    reality

---
+++ app/Http/Controllers/RealityController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Budget;
+use App\Models\BudgetLine;
+use App\Models\Expense;
+
+
+class RealityController extends Controller
+{
+    public function budgetReality(){
+
+    }
+
+    public function budgetLineReality(){
+
+    }
+    
+}
