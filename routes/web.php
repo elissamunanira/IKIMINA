60efbd80274f104c5fc3e63feb0e2e77164045d0
elissamunanira <munanira64@gmail.com>
2023-05-26 08:30:38 +0200

    homepage

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\{UserController,RoleController,DashboardController,Savi
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home.index');
 });
 
 
