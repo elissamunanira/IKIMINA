32df6f9bb5bb420373bee413742dce53ef42e8e3
elissamunanira <munanira64@gmail.com>
2023-06-19 17:13:13 +0200

    mituelle

---
+++ resources/views/mituelle/index.blade.php
@@ -0,0 +1,70 @@
+@extends('dashboard.app')
+
+@section('content')
+<div class="content-wrap">
+    <div class="main">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-8 p-r-0 title-margin-right">
+                    <div class="page-header">
+                        <div class="page-title">
+                            <h1>Hello, <span>Welcome to Saving Management Field</span></h1>
+                        </div>
+                    </div>
+                </div>
+                <!-- /# column -->
+                <div class="col-lg-4 p-l-0 title-margin-left">
+                    <div class="page-header">
+                        <div class="page-title">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="/dash">Dashboard</a></li>
+                                <li class="breadcrumb-item active">Savings</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+                <!-- /# column -->
+            </div>
+            <!-- /# row -->
+            <section id="main-content">
+                <div class="row">
+                    <div class="col-lg-12"> 
+                        <h3>Saving records for <strong>{{ $user->firstname }} {{ $user->lastname }}</strong></h3>
+                        <div class="card">
+                            <div class="container">
+                                
+                                <div class="bootstrap-data-table-panel">
+                                    <div class="table-responsive">
+                                        <table id="bootstrap-data-table-export" class="table table-striped table-bordered">
+                                            <thead>
+                                                <tr>
+                                                    <th>#</th>
+                                                    <th>Date</th>
+                                                    <th>Month Paid</th>
+                                                    <th>Amount</th>
+                                                    <th>Action</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                @foreach ($savings as $saving)
+                                                    <tr>
+                                                        <td>{{++$i}}</td>
+                                                        <td>{{ $saving->created_at }}</td>
+                                                        <td>{{ $saving->month }}</td>
+                                                        <td>{{ $saving->amount }} RWF</td>
+                                                        <td><a href="/saving/{{ $saving->id }}/edit">Edit</a></td>
+                                                    </tr>
+                                                @endforeach
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                </div>
+            </section>
+        </div>
+    </div>
+</div>
+@endsection
