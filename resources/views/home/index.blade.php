be048e02d90b47114e1f9b4f4511c5f9c4ef0a34
elissamunanira <munanira64@gmail.com>
2023-07-03 18:09:03 +0200

    report

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -79,7 +79,7 @@
                         </div>
                     </div>
                     <a href="contact.html" class="nav-item nav-link">Contact</a>
-                    <a href="contact.html" class="nav-item nav-link">Login/Register</a>
+                    <a href="/login" class="nav-item nav-link">Login/Register</a>
                 </div>
                 <div class="d-none d-lg-flex ms-2">
                     <a class="btn btn-light btn-sm-square rounded-circle ms-3" href="">
