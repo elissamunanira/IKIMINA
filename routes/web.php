504b68782a8b28cc827d8c8edcdbff09d5297161
elissamunanira <munanira64@gmail.com>
2023-11-16 16:59:08 +0200

    forgetpassword

--- routes/web.php
+++ routes/web.php
@@ -70,8 +70,8 @@ Route::post('/register',[UserController::class, 'register']);
 
 Route::get('/logout',[UserController::class, 'logout']);
 
-Route::get('/forget-passwordsss',[ForgetPasswordManager::class, 'forgetPassword']);
-Route::post('/forget-passwords',[ForgetPasswordManager::class, 'forgetPasswordPost']);
+Route::get('/forget-password',[ForgetPasswordManager::class, 'forgetPassword']);
+Route::post('/forget-password',[ForgetPasswordManager::class, 'forgetPasswordPost']);
 
 
 
