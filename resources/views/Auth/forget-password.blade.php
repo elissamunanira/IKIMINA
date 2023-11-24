738fa6b8b53085821c83dd06c1a5bce51f162c38
elissamunanira <munanira64@gmail.com>
2023-11-24 07:28:00 +0200

    forget-password

--- resources/views/Auth/forget-password.blade.php
+++ resources/views/Auth/forget-password.blade.php
@@ -1,8 +1,8 @@
 @extends('Auth.app')
 @section('content')
+
      <h3 class="mb-4 pb-2 pb-md-0 mb-md-5">Forget Password</h3>
+     <form action="{{route('forget.password.post')}}" method="POST">
+     </form>
 
-
-
-      
 @endsection
