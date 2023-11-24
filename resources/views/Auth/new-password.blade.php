6112856297caa1f9f77abfd238ed9ca75cc7b789
elissamunanira <munanira64@gmail.com>
2023-11-24 12:03:28 +0200

    forget-password

--- resources/views/Auth/new-password.blade.php
+++ resources/views/Auth/new-password.blade.php
@@ -10,22 +10,37 @@
     </div>
     @endif
 
-     <h3 class="mb-4 pb-2 pb-md-0 mb-md-5 text-center">Forget Password</h3>
+     <h3 class="mb-4 pb-2 pb-md-0 mb-md-5 text-center">Reseting Password</h3>
      <form action="{{route('forget.password.post')}}" method="POST">
           @csrf
           <div class="row">
                <div class="col-md-12 mb-4 pb-2">
-                    <p>We will send a link to your Email, so use that link to reset your password</p>
-                 <div class="form-outline">
-                   <input type="email" name="email" class="form-control form-control-lg" />
-                   <label class="form-label" for="emailAddress">Email</label>
-                 </div>
-     
-                 @if($errors->has('email'))
-                 <span class="text-danger">
-                   {{$errors->first('email')}}
-                 </span>
-                 @endif
+                    <p>Please create new password</p>
+                    <div class="row">
+                        <div class="col-md-6 mb-4 pb-2">
+              
+                          <div class="form-outline">
+                            <input type="password" name="password" class="form-control form-cont" />
+                            <label class="form-label"for="password" >Password</label>
+                          </div>
+              
+                          @if($errors->has('password'))
+                          <span class="text-danger">
+                            {{$errors->first('password')}}
+                          </span>
+                          @endif
+              
+                        </div>
+                        <div class="col-md-6 mb-4 pb-2">
+              
+                          <div class="form-outline">
+                            <input type="password" name="password_confirmation" class="form-control form-cont" />
+                            <label class="form-label" for="password" >Confirm Password</label>
+                          </div>
+              
+                        </div>
+                      </div>
+               </div>
      
                </div>
                
