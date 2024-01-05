6e18166df102c92b7dbfaf46af5c64874d15d972
elissamunanira <munanira64@gmail.com>
2024-01-05 15:42:26 +0200

    importing file

--- app/Http/Controllers/SavingController.php
+++ app/Http/Controllers/SavingController.php
@@ -103,108 +103,37 @@ class SavingController extends Controller
         return view('savings.import');
     }
 
-    public function uploadContent(){
-        Excel::import(new SavingImport, request()->file('file'));
 
-        return redirect()->back();
-    }
+    public function import()
+    {
+        try {
+            $spreadsheet = IOFactory::load('path/to/your_excel_file.xlsx');
+            $worksheet = $spreadsheet->getActiveSheet();
+            $data = [];
 
-    // public function uploadContent(Request $request)
-    //     {
+            foreach ($worksheet->getRowIterator() as $row) {
+                $rowData = [];
+                foreach ($row->getCellIterator() as $cell) {
+                    $rowData[] = $cell->getValue();
+                }
+                $data[] = $rowData;
+            }
 
-    //     $file = $request->file('uploaded_file');
+            // Now $data contains the Excel data. You can insert it into the database as needed.
 
-    //     if ($file) {
-            
-    //     $filename = $file->getClientOriginalName();
-    //     $extension = $file->getClientOriginalExtension(); //Get extension of uploaded file
-    //     $tempPath = $file->getRealPath();
-    //     $fileSize = $file->getSize(); //Get size of uploaded file in bytes
-    //     //Check for file extension and size
-    //     $this->checkUploadedFileProperties($extension, $fileSize);
-    //     //Where uploaded file will be stored on the server 
-    //     $location = 'uploads'; //Created an "uploads" folder for that
-    //     // Upload file
-    //     $file->move($location, $filename);
-    //     // In case the uploaded file path is to be stored in the database 
-    //     $filepath = public_path($location . "/" . $filename);
-    //     // Reading file
-    //     $file = fopen($filepath, "r");
-    //     $importData_arr = array(); // Read through the file and store the contents as an array
-    //     $i = 0;
-    //     //Read the contents of the uploaded file 
-    //     while (($filedata = fgetcsv($file, 1000, ",")) !== FALSE) {
-    //     $num = count($filedata);
-    //     // Skip first row (Remove below comment if you want to skip the first row)
-    //     if ($i == 0) {
-    //     $i++;
-    //     continue;
-    //     }
-    //     for ($c = 0; $c < $num; $c++) {
-    //     $importData_arr[$i] = $filedata[$c];
-    //     }
-    //     $i++;
-    //     }
-    //     fclose($file); //Close after reading
-    //     $j = 0;
-    //     foreach ($importData_arr as $importData) {
-    //     // $user_id = $importData[1]; //Get user names
-    //     // $email = $importData[3]; //Get the user emails
-    //     $j++;
-    //     try {
-    //     DB::beginTransaction();
+            // Example: Insert into a hypothetical "items" table
+            foreach ($data as $row) {
+                DB::table('items')->insert([
+                    'column1' => $row[0],
+                    'column2' => $row[1],
+                    // Add more columns as needed
+                ]);
+            }
 
-    //     Saving::create([
-    //     'user_id' => $importData[1],
-    //     'amount' => $importData[2],
-    //     'date' => $importData[3]
-    //     ]);
-
-    //     //Send Email
-    //     $this->sendEmail($email, $name);
-    //     DB::commit();
-    //     } 
-        
-    //     catch (\Exception $e) {
-    //     //throw $th;
-    //     DB::rollBack();
-    //     }
-    //     }
-
-    //     return response()->json([
-    //     'message' => "$j records successfully uploaded"
-    //     ]);
-
-    //     } else {
-    //     //no file was uploaded
-    //     throw new \Exception('No file was uploaded', Response::HTTP_BAD_REQUEST);
-
-    //     // return view('savings.import')->with('success','No file was uploaded');
-
-    //     }
-    //     }
-    //     public function checkUploadedFileProperties($extension, $fileSize)
-    //     {
-            
-    //     $valid_extension = array("csv", "xlsx"); //Only want csv and excel files
-    //     $maxFileSize = 2097152; // Uploaded file size limit is 2mb
-
-    //     if (in_array(strtolower($extension), $valid_extension)) {
-
-    //     if ($fileSize <= $maxFileSize) {
-    //     } 
-        
-    //     else {
-    //     throw new \Exception('No file was uploaded', Response::HTTP_REQUEST_ENTITY_TOO_LARGE); //413 error
-    //     }
-
-    //     } 
-        
-    //     else {
-    //     throw new \Exception('Invalid file extension', Response::HTTP_UNSUPPORTED_MEDIA_TYPE); //415 error
-    //     }
-    //     }
-
-    
+            return redirect()->route('your.route')->with('success', 'Data imported successfully!');
+        } catch (\Exception $e) {
+            return redirect()->route('your.route')->with('error', 'Error importing data: ' . $e->getMessage());
+        }
+    }    
 
 }
